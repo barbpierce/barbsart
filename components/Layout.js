3d089f90681e46c952526edb81b88d6a31f7593f
Matthew Pierce <lesbianhaircut@outlook.com>
2022-09-04 20:23:49 -0400

    c

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,6 @@
 import Navbar from "./Navbar";
-import Footer from "./Footer/index";
-import styled from 'styled-components';
+import Footer from "../components/footer/index";
+import styled from "styled-components";
 const Cont = styled.div`
   margin: 0 5%;
 `;
